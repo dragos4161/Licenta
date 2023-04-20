484561602e8bc3c21c8a196f4e22f8a025ac8344
dragos4161 <dima.dragos0@gmail.com>
2023-04-20 15:52:59 +0300

    Added floating button and bottom navigation

--- MobileApp/city_problems/lib/presentation/home_page.dart
+++ MobileApp/city_problems/lib/presentation/home_page.dart
@@ -10,9 +10,41 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
+    return Scaffold(
+      body: const Center(
         child: Text("Salutare!"),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        shape: BeveledRectangleBorder(borderRadius: BorderRadius.circular(15)),
+        elevation: 20,
+        backgroundColor: const Color.fromRGBO(80, 151, 255, 1),
+        splashColor: Colors.blue,
+        child: const Icon(Icons.add),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      bottomNavigationBar: BottomAppBar(
+        shape: const CircularNotchedRectangle(),
+        notchMargin: 10,
+        child: SizedBox(
+          height: 60,
+          child: Padding(
+            padding: const EdgeInsets.fromLTRB(30, 0, 30, 0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                MaterialButton(
+                  onPressed: () {},
+                  child: const Text("1"),
+                ),
+                MaterialButton(
+                  onPressed: () {},
+                  child: const Text("2"),
+                ),
+              ],
+            ),
+          ),
+        ),
       ),
     );
   }
