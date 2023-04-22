2934d476b108c9764261ee1b44861621acfe1de2
dragos4161 <dima.dragos0@gmail.com>
2023-04-22 16:44:19 +0300

    Created the initializeUserSucessful reducer

--- MobileApp/city_problems/lib/reducer/auth_reducer.dart
+++ MobileApp/city_problems/lib/reducer/auth_reducer.dart
@@ -4,8 +4,15 @@ import 'package:redux/redux.dart';
 
 Reducer<AuthState> authReducer = combineReducers(<Reducer<AuthState>>[
   TypedReducer<AuthState,LoginSuccessful>(_loginSuccessful),
+  TypedReducer<AuthState, InitializeUserSuccessful>(_initializeUserSuccessful),
 ]);
 AuthState _loginSuccessful(AuthState state, LoginSuccessful action) {
+  return state.copyWith(
+    user: action.user,
+  );
+}
+
+AuthState _initializeUserSuccessful(AuthState state, InitializeUserSuccessful action) {
   return state.copyWith(
     user: action.user,
   );
