188aa665030898fb59c01a4711127b448919369c
dragos4161 <dima.dragos0@gmail.com>
2023-04-22 17:54:14 +0300

    Wrapped the button in a Builder widget

--- MobileApp/city_problems/lib/presentation/login.dart
+++ MobileApp/city_problems/lib/presentation/login.dart
@@ -1,5 +1,9 @@
+import 'package:city_problems/actions/index.dart';
+import 'package:city_problems/models/index.dart';
+import 'package:city_problems/presentation/containers/user_container.dart';
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_redux/flutter_redux.dart';
 
 class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
@@ -13,142 +17,148 @@ class _LoginPageState extends State<LoginPage> {
   Widget build(BuildContext context) {
     final TextEditingController emailController = TextEditingController();
     final TextEditingController passwordController = TextEditingController();
-    return Scaffold(
-      resizeToAvoidBottomInset: false,
-      body: Padding(
-        padding: const EdgeInsets.fromLTRB(50, 80, 50, 0),
-        child: Center(
-          child: Column(
-            children: <Widget>[
-              const Text(
-                'Login',
-                style: TextStyle(
-                  color: Color.fromRGBO(80, 151, 255, 1),
-                  fontSize: 60,
-                  fontFamily: 'ebrima',
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              const SizedBox(
-                height: 50,
-              ),
-              Material(
-                elevation: 5,
-                shadowColor: const Color.fromRGBO(80, 151, 255, 1),
-                borderRadius: BorderRadius.circular(25),
-                child: TextField(
-                  controller: emailController,
-                  keyboardType: TextInputType.emailAddress,
-                  decoration: InputDecoration(
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(25),
-                      borderSide: BorderSide.none,
-                    ),
-                    filled: true,
-                    hintText: 'e-mail',
-                    hintStyle: const TextStyle(
-                      color: Color.fromRGBO(132, 150, 155, 1),
-                      fontFamily: 'ebrima',
-                      fontWeight: FontWeight.bold,
-                    ),
-                    fillColor: Colors.white,
-                  ),
-                ),
-              ),
-              const SizedBox(
-                height: 50,
-              ),
-              Material(
-                elevation: 5,
-                shadowColor: const Color.fromRGBO(80, 151, 255, 1),
-                borderRadius: BorderRadius.circular(25),
-                child: TextField(
-                  controller: passwordController,
-                  keyboardType: TextInputType.text,
-                  obscureText: true,
-                  decoration: InputDecoration(
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(25),
-                      borderSide: BorderSide.none,
-                    ),
-                    filled: true,
-                    hintText: 'password',
-                    hintStyle: const TextStyle(
-                      color: Color.fromRGBO(132, 150, 155, 1),
-                      fontFamily: 'ebrima',
-                      fontWeight: FontWeight.bold,
-                    ),
-                    fillColor: Colors.white,
-                  ),
-                ),
-              ),
-              const SizedBox(
-                height: 30,
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
+    return UserContainer(
+      builder: (BuildContext context, AppUser? appUser) {
+        return Scaffold(
+          resizeToAvoidBottomInset: false,
+          body: Padding(
+            padding: const EdgeInsets.fromLTRB(50, 80, 50, 0),
+            child: Center(
+              child: Column(
                 children: <Widget>[
-                  RichText(
-                    text: TextSpan(
-                      style: const TextStyle(
-                        color: Color.fromRGBO(103, 169, 249, 1),
-                        fontFamily: 'ebrima',
-                        fontWeight: FontWeight.bold,
-                        fontSize: 14,
-                      ),
-                      children: <TextSpan>[
-                        const TextSpan(text: "Haven't account?  "),
-                        TextSpan(
-                          text: 'Sign-up',
-                          style: const TextStyle(
-                            color: Color.fromRGBO(50, 135, 237, 1),
-                            fontSize: 16,
-                          ),
-                          recognizer: TapGestureRecognizer()
-                            ..onTap = () {
-                              Navigator.pushNamed(context, '/signup');
-                            },
-                        ),
-                      ],
+                  const Text(
+                    'Login',
+                    style: TextStyle(
+                      color: Color.fromRGBO(80, 151, 255, 1),
+                      fontSize: 60,
+                      fontFamily: 'ebrima',
+                      fontWeight: FontWeight.bold,
                     ),
                   ),
+                  const SizedBox(
+                    height: 50,
+                  ),
+                  Material(
+                    elevation: 5,
+                    shadowColor: const Color.fromRGBO(80, 151, 255, 1),
+                    borderRadius: BorderRadius.circular(25),
+                    child: TextField(
+                      controller: emailController,
+                      keyboardType: TextInputType.emailAddress,
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(25),
+                          borderSide: BorderSide.none,
+                        ),
+                        filled: true,
+                        hintText: 'e-mail',
+                        hintStyle: const TextStyle(
+                          color: Color.fromRGBO(132, 150, 155, 1),
+                          fontFamily: 'ebrima',
+                          fontWeight: FontWeight.bold,
+                        ),
+                        fillColor: Colors.white,
+                      ),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 50,
+                  ),
+                  Material(
+                    elevation: 5,
+                    shadowColor: const Color.fromRGBO(80, 151, 255, 1),
+                    borderRadius: BorderRadius.circular(25),
+                    child: TextField(
+                      controller: passwordController,
+                      keyboardType: TextInputType.text,
+                      obscureText: true,
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(25),
+                          borderSide: BorderSide.none,
+                        ),
+                        filled: true,
+                        hintText: 'password',
+                        hintStyle: const TextStyle(
+                          color: Color.fromRGBO(132, 150, 155, 1),
+                          fontFamily: 'ebrima',
+                          fontWeight: FontWeight.bold,
+                        ),
+                        fillColor: Colors.white,
+                      ),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 30,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      RichText(
+                        text: TextSpan(
+                          style: const TextStyle(
+                            color: Color.fromRGBO(103, 169, 249, 1),
+                            fontFamily: 'ebrima',
+                            fontWeight: FontWeight.bold,
+                            fontSize: 14,
+                          ),
+                          children: <TextSpan>[
+                            const TextSpan(text: "Haven't account?  "),
+                            TextSpan(
+                              text: 'Sign-up',
+                              style: const TextStyle(
+                                color: Color.fromRGBO(50, 135, 237, 1),
+                                fontSize: 16,
+                              ),
+                              recognizer: TapGestureRecognizer()
+                                ..onTap = () {
+                                  Navigator.pushNamed(context, '/signup');
+                                },
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                  const SizedBox(
+                    height: 50,
+                  ),
+                  Builder(
+                    builder: (BuildContext context) {
+                      return ElevatedButton(
+                        onPressed: () async{
+                          final Login action = Login(
+                            email: emailController.text,
+                            password: passwordController.text,
+                          );
+                          StoreProvider.of<AppState>(context).dispatch(action);
+                        },
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: const Color.fromRGBO(80, 151, 255, 1),
+                        ),
+                        child: const SizedBox(
+                          width: 200,
+                          height: 60,
+                          child: Center(
+                            child: Text(
+                              'LOGIN',
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 35,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ),
+                        ),
+                      );
+                    },
+                  )
                 ],
               ),
-              const SizedBox(
-                height: 50,
-              ),
-              ElevatedButton(
-                onPressed: () {
-                  ScaffoldMessenger.of(context).showSnackBar(
-                    SnackBar(
-                      content: Text(
-                        emailController.text + passwordController.text,
-                      ),
-                    ),
-                  );
-                },
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: const Color.fromRGBO(80, 151, 255, 1),
-                ),
-                child: const SizedBox(
-                  width: 200,
-                  height: 60,
-                  child: Center(
-                    child: Text(
-                      'LOGIN',
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 35,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                  ),
-                ),
-              )
-            ],
+            ),
           ),
-        ),
-      ),
+        );
+      },
     );
   }
 }
