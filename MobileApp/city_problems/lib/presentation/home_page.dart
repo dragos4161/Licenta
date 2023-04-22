bb67b1c9333b0254ebe6362f4aa396d146b35ef0
dragos4161 <dima.dragos0@gmail.com>
2023-04-22 17:53:27 +0300

    Wrapped the button in a Builder

--- MobileApp/city_problems/lib/presentation/home_page.dart
+++ MobileApp/city_problems/lib/presentation/home_page.dart
@@ -1,5 +1,7 @@
 import 'dart:async';
 
+import 'package:city_problems/models/index.dart';
+import 'package:city_problems/presentation/containers/user_container.dart';
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
@@ -19,95 +21,104 @@ class _HomePageState extends State<HomePage> {
     zoom: 14.4746,
   );
 
+  @override
+  void initState(){
+    super.initState();
+  }
+
   final TextEditingController _searchController = TextEditingController();
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: SizedBox(
-        height: MediaQuery.of(context).size.height,
-        width: MediaQuery.of(context).size.width,
-        child: Stack(
-          children: <Widget>[
-            GoogleMap(
-              initialCameraPosition: _kGooglePlex,
-              mapType: MapType.hybrid,
-              onMapCreated: (GoogleMapController controller) {
-                _controller.complete(controller);
-              },
-            ),
-            Positioned(
-              top: 70,
-              left: 65,
-              right: 65,
-              child: Container(
-                height: MediaQuery.of(context).size.height * 0.06,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(15),
-                  color: Colors.white,
+    return UserContainer(
+      builder: (BuildContext context, AppUser? user) {
+        return Scaffold(
+          body: SizedBox(
+            height: MediaQuery.of(context).size.height,
+            width: MediaQuery.of(context).size.width,
+            child: Stack(
+              children: <Widget>[
+                GoogleMap(
+                  initialCameraPosition: _kGooglePlex,
+                  mapType: MapType.hybrid,
+                  onMapCreated: (GoogleMapController controller) {
+                    _controller.complete(controller);
+                  },
                 ),
-                child: Expanded(
-                  child: TextField(
-                    controller: _searchController,
-                    decoration: InputDecoration(
-                      border: InputBorder.none,
-                      contentPadding: const EdgeInsets.all(10),
-                      suffixIcon: GestureDetector(
-                        onTap: () {
-                          FocusManager.instance.primaryFocus?.unfocus();
-                        },
-                        child: const Icon(Icons.filter_alt_outlined),
-                      ),
-                      labelText: 'Categorie',
-                      labelStyle: const TextStyle(
-                        color: Colors.grey,
-                        fontSize: 15,
+                Positioned(
+                  top: 70,
+                  left: 65,
+                  right: 65,
+                  child: Container(
+                    height: MediaQuery.of(context).size.height * 0.06,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(15),
+                      color: Colors.white,
+                    ),
+                    child: Expanded(
+                      child: TextField(
+                        controller: _searchController,
+                        decoration: InputDecoration(
+                          border: InputBorder.none,
+                          contentPadding: const EdgeInsets.all(10),
+                          suffixIcon: GestureDetector(
+                            onTap: () {
+                              FocusManager.instance.primaryFocus?.unfocus();
+                            },
+                            child: const Icon(Icons.filter_alt_outlined),
+                          ),
+                          labelText: user == null ? 'Category':user.email,
+                          labelStyle: const TextStyle(
+                            color: Colors.grey,
+                            fontSize: 15,
+                          ),
+                        ),
                       ),
                     ),
                   ),
-                ),
-              ),
-            )
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {},
-        shape: const CircleBorder(),
-        elevation: 20,
-        backgroundColor: const Color.fromRGBO(80, 151, 255, 1),
-        splashColor: Colors.blue,
-        child: const Icon(
-          Icons.add,
-          size: 35,
-        ),
-      ),
-      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
-      bottomNavigationBar: BottomAppBar(
-        shape: const CircularNotchedRectangle(),
-        elevation: 20,
-        notchMargin: 10,
-        child: SizedBox(
-          height: 60,
-          child: Padding(
-            padding: const EdgeInsets.fromLTRB(30, 0, 30, 0),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: <Widget>[
-                MaterialButton(
-                  onPressed: () {},
-                  splashColor: Colors.white,
-                  child: const Icon(Icons.person_outline_rounded),
-                ),
-                MaterialButton(
-                  onPressed: () {},
-                  splashColor: Colors.white,
-                  child: const Icon(Icons.messenger_outline_rounded),
-                ),
+                )
               ],
             ),
           ),
-        ),
-      ),
+          floatingActionButton: FloatingActionButton(
+            onPressed: () {},
+            shape: const CircleBorder(),
+            elevation: 20,
+            backgroundColor: const Color.fromRGBO(80, 151, 255, 1),
+            splashColor: const Color.fromRGBO(11, 68, 112, 1),
+            child: const Icon(
+              Icons.add,
+              size: 35,
+            ),
+          ),
+          floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+          bottomNavigationBar: BottomAppBar(
+            shape: const CircularNotchedRectangle(),
+            elevation: 20,
+            notchMargin: 10,
+            child: SizedBox(
+              height: 60,
+              child: Padding(
+                padding: const EdgeInsets.fromLTRB(30, 0, 30, 0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    MaterialButton(
+                      onPressed: () {},
+                      splashColor: Colors.white,
+                      child: const Icon(Icons.person_outline_rounded),
+                    ),
+                    MaterialButton(
+                      onPressed: () {},
+                      splashColor: Colors.white,
+                      child: const Icon(Icons.messenger_outline_rounded),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        );
+      },
     );
   }
 }
