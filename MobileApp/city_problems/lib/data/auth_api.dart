4c8a59060512372c5ea7eab421380b395892f6ff
dragos4161 <dima.dragos0@gmail.com>
2023-05-03 22:02:27 +0300

    Created the function that gets the current location

--- MobileApp/city_problems/lib/data/auth_api.dart
+++ MobileApp/city_problems/lib/data/auth_api.dart
@@ -1,5 +1,6 @@
 import 'package:city_problems/models/index.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:location/location.dart';
 
 class AuthApi {
   const AuthApi({required this.auth});
@@ -42,6 +43,32 @@ class AuthApi {
       email: user.email!,
       displayName: user.displayName,
     );
+  }
+
+  Future<DangerLocation?> getLocation() async {
+    final Location location = Location();
+
+    late bool serviceEnabled;
+    late PermissionStatus permissionGranted;
+    late LocationData locationData;
+
+    serviceEnabled = await location.serviceEnabled();
+    if (!serviceEnabled) {
+      serviceEnabled = await location.requestService();
+      if (!serviceEnabled) {
+        return null;
+      }
+    }
+
+    permissionGranted = await location.hasPermission();
+    if (permissionGranted == PermissionStatus.denied) {
+      permissionGranted = await location.requestPermission();
+      if (permissionGranted != PermissionStatus.granted) {
+        return null;
+      }
+    }
+    locationData = await location.getLocation();
+    return DangerLocation(latitude: locationData.longitude!, longitude: locationData.latitude!);
   }
 
   Future<void> logout() async {
