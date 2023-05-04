b962b512ea676b314f5fc1f7a506c20b65618c71
dragos4161 <dima.dragos0@gmail.com>
2023-05-04 20:58:56 +0300

    Created the epic for getLocation action

--- MobileApp/city_problems/lib/epics/app_epics.dart
+++ MobileApp/city_problems/lib/epics/app_epics.dart
@@ -1,10 +1,11 @@
+import 'package:city_problems/actions/index.dart';
 import 'package:city_problems/data/auth_api.dart';
 import 'package:city_problems/epics/auth_epics.dart';
-import 'package:city_problems/epics/danger_epic.dart';
 import 'package:city_problems/models/index.dart';
 import 'package:redux_epics/redux_epics.dart';
+import 'package:rxdart/transformers.dart';
 
-class AppEpics{
+class AppEpics {
   const AppEpics({required this.authApi});
 
   final AuthApi authApi;
@@ -13,8 +14,17 @@ class AppEpics{
     return combineEpics(
       <Epic<AppState>>[
         AuthEpics(api: authApi).epic,
-        DangerEpics(api: authApi).epic,
+        TypedEpic<AppState, GetLocationStart>(_getLocationStart),
       ],
     );
+  }
+
+  Stream<dynamic> _getLocationStart(Stream<GetLocationStart> actions, EpicStore<AppState> store) {
+    return actions.flatMap((GetLocationStart action) {
+      return Stream<void>.value(null)
+          .asyncMap((_) => authApi.getLocation())
+          .map((CurrentLocation? location) => GetLocation.successful(location!))
+          .onErrorReturnWith((Object error, StackTrace stackTrace) => GetLocation.error(error, stackTrace));
+    });
   }
 }
